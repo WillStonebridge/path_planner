7cdbf0da2b26748c372fa935a5ff2e06e332e3b8
leonardjmihs <lennyjung5@gmail.com>
2020-12-28 12:52:28 -0800

    added

--- PathPlanner.py
+++ PathPlanner.py
@@ -103,22 +103,21 @@ class PathPlanner:
             parent_index = node.parent_index
         return rough_path_lat, rough_path_lon
     
-    def calc_path(self):
+    def calc_path(self, swaypoint, gwaypoint):
         finalpathx = []
         finalpathy = []
-        for i in range(len(self.waypoints) - 1):
-            start_time = time.time()
-            start_node = self.Node(self.map.transform_to_map_position(self.waypoints[i][0], self.map.min_lat), self.map.transform_to_map_position(self.waypoints[i][1], self.map.min_lon), 0.0, -1)
-            goal_node = self.Node(self.map.transform_to_map_position(self.waypoints[i + 1][0], self.map.min_lat), self.map.transform_to_map_position(self.waypoints[i + 1][1], self.map.min_lon), 0.0, -1)
-            x, y = self.a_star(start_node, goal_node)
-            ex, ey = self.calc_critical_nodes(x,y)
-            fx, fy = self.calc_smooth_line(ex, ey)
-            finalpathx += (fx) 
-            finalpathy += (fy)
-            print(f"time for leg {i + 1}: {time.time() - start_time}")
-            plt.plot(x,y, '-r')
-            plt.plot(ex,ey, '.b')
-            plt.plot(fx, fy, 'g-s')
+        start_time = time.time()
+        start_node = self.Node(self.map.transform_to_map_position(swaypoint[0], self.map.min_lat), self.map.transform_to_map_position(swaypoint[1], self.map.min_lon), 0.0, -1)
+        goal_node = self.Node(self.map.transform_to_map_position(gwaypoint[0], self.map.min_lat), self.map.transform_to_map_position(gwaypoint[1], self.map.min_lon), 0.0, -1)
+        x, y = self.a_star(start_node, goal_node)
+        ex, ey = self.calc_critical_nodes(x,y)
+        fx, fy = self.calc_smooth_line(ex, ey)
+        finalpathx += (fx) 
+        finalpathy += (fy)
+        print(f"time for leg: {time.time() - start_time}")
+        plt.plot(x,y, '-r')
+        plt.plot(ex,ey, '.b')
+        plt.plot(fx, fy, 'g-s')
         return finalpathx, finalpathy
     # def calc_path(self):
     #     finalpathx = []
@@ -197,7 +196,8 @@ def main():
     resolution = 1
     buffer = 3
     plan = PathPlanner(resolution, mission_data, buffer)
-    fx, fy = plan.calc_path()
+    for i in range(len(plan.waypoints) - 1):
+        fx, fy = plan.calc_path(plan.waypoints[i], plan.waypoints[i+1])
     invalid = []
     for x in range(len(plan.map.obstacle_map)):
         for y in range(len(plan.map.obstacle_map[x])):
